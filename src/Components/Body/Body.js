e96d0a14be11a4fe0d065001da9351a1851aa789
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-01-14 21:26:00 +0000

    set up files

---
+++ src/Components/Body/Body.js
@@ -0,0 +1,11 @@
+import './Body.css'
+
+const Body = () => {
+  return (
+    <div>
+      BODY
+    </div>
+  )
+}
+
+export default Body
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div>
+      HEADER
+    </div>
+  )
+}
+
+export default Header;

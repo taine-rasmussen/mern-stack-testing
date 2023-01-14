e19139f8cd28e022ba04662fbc07f82dba6c55f1
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-01-14 22:33:22 +0000

    formatting

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const mongodb = require('mongodb');
 const app = express()
 const port = 3000
+
+let MongoClient = mongodb.MongoClient;
+
+MongoClient.connect("mongodb://localhost:27017/mern-project", function(err, database) {
+  if (err) return console.error(err);
+console.log("MongoDB connected on port 27017")
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World, This is MERN Stack Project!')
 })
 
 app.listen(port, () => {
-  console.log(`ITS WORKING!!!! http://localhost:${port}`)
+  console.log(`WORKING AT http://localhost:${port}`)
 })

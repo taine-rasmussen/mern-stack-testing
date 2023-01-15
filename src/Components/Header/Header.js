48e2c6e2ca87180771c892c2ed95a57c46bbd88c
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-01-15 22:13:19 +0000

    headerlol

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,13 @@
 import './Header.css'
+import { AiFillCheckCircle } from 'react-icons/ai'
+import { IoMdClose } from 'react-icons/io'
 
 const Header = () => {
   return (
     <div className="header_container">
-      HEADER
+      <AiFillCheckCircle size={25} />
+      ToDo
+      <IoMdClose size={25} />
     </div>
   )
 }

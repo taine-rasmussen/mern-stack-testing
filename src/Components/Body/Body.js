70d77df0297e30c2d8dc3242aa939e086b0b0c25
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-01-15 22:00:51 +0000

    get items from api

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -1,9 +1,33 @@
 import './Body.css'
+import { useState, useEffect } from 'react'
+import Axios from 'axios'
+import axios from 'axios';
 
 const Body = () => {
+
+  const [items, setItems] = useState([]);
+
+  useEffect(
+    () => {
+      axios.get("http://localhost:3001/getList")
+      .then((res) => {
+        setItems(res.data)
+      })
+    }, [items]
+  )
+
   return (
     <div>
       BODY
+      <div className="list-display">
+        {items.map((item, i) => {
+          return(
+            <div key={item.title}>
+              {item.title}
+            </div>  
+          )
+        })}
+      </div>
     </div>
   )
 }

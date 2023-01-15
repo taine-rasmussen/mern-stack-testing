15c78ccb439f76e56daf0042714b4fda76b7ca79
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-01-15 13:27:37 +0000

    wire up db

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,8 @@
-const express = require('express')
+const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://TaineRas:Flick001%3D%3D1@cluster0.orhpsyz.mongodb.net/merntesting?retryWrites=true&w=majority')
 
 app.listen(3001, () => {
   console.log('server is running')
